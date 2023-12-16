b43c9f187ed3b0de7da11c67b73aadf8e4e83d5f
Georg Dronov <dronovgeorg72@gmail.com>
2023-12-16 21:11:34 +0300

    Задача 2

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,57 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+
+    this.body = this.elem.querySelector('.modal__body');
+  }
+
+  addEventListeners() {
+    this.elem.querySelector('.modal__close').addEventListener('click', () => this.close());
+    document.addEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  open() {
+    document.body.appendChild(this.elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  setTitle(title) {
+    this.elem.querySelector('.modal__title').textContent = title;
+  }
+
+  setBody(node) {
+    this.body.innerHTML = '';
+    this.body.appendChild(node);
+  }
+
+  close() {
+    document.body.removeChild(this.elem);
+    document.body.classList.remove('is-modal-open');
+    document.removeEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  handleKeyDown(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
   }
 }
--- 7-module/2-task/task.test.js
+++ 7-module/2-task/task.test.js
@@ -1,207 +1,58 @@
-import Modal from './index.js';
-
-describe('7-module-2-task', () => {
-  let modal;
-
-  beforeEach(() => {
-    modal = new Modal();
-  });
-
-  afterEach(() => {
-    modal.close();
-  });
-
-  describe('метод open()', () => {
-    beforeEach(() => {
-      modal.open();
-    });
-
-    afterEach(() => {
-      modal.close();
-    });
-
-    it('должна создавать основной элемент модального окна', () => {
-      let modalElement = document.body.querySelector('.modal');
-
-      expect(modalElement).toBeTruthy();
-    });
-
-    it('должна создавать прозрачный фон', () => {
-      let modalElement = document.body.querySelector('.modal .modal__overlay');
-
-      expect(modalElement).toBeTruthy();
-    });
-
-    it('должна добавлять класс "is-modal-open" элементу body', () => {
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
-
-      expect(hasIsModalOpenClass).toBe(true);
-    });
-  });
-
-  describe(`метод setTitle('modal title')`, () => {
-    it('должен задавать заголовок модального окна', () => {
-      let title = 'Я главное модальное окно';
-
-      modal.setTitle(title);
-      modal.open();
-
-      let titleElement = document.querySelector('.modal__title');
-
-      expect(titleElement.textContent.trim()).toBe(title);
-
-      modal.close();
-    });
-
-    it('должен задавать заголовок модального окна после его открытия', () => {
-      let title = 'Я главное модальное окно';
-
-      modal.open();
-
-      modal.setTitle(title);
-
-      let titleElement = document.querySelector('.modal__title');
-
-      expect(titleElement.textContent.trim()).toBe(title);
-
-      modal.close();
-    });
-  });
-
-  describe(`метод setBody(node)`, () => {
-    const createModalBody = (text) => {
-      let node = document.createElement('div');
-      node.className = 'test-node-class';
-      node.innerHTML = `<h4>${text}</h4>`;
-
-      return node;
-    };
-
-    const selectInnerTitle = () => {
-      let modalBody = document.querySelector('.modal .modal__body');
-      let nodeFromModal = modalBody.querySelector('.test-node-class');
-      let innerTitle = nodeFromModal && nodeFromModal.querySelector('h4');
-
-      return innerTitle.textContent;
-    };
-
-    afterEach(() => {
-      modal.close();
-    });
-
-    it('должен вставлять содержимое модального окна в элемента с классом "modal__body"', () => {
-      let innerTitleText = 'Внутренний заголовок содержимого';
-      const node = createModalBody(innerTitleText);
-
-      modal.setBody(node);
-
-      modal.open();
-
-      expect(selectInnerTitle()).toBe(innerTitleText);
-    });
-
-    it('должен вставлять содержимое модального окна в элемента с классом "modal__body" после его открытия', () => {
-      modal.open();
-
-      let innerTitleText = 'Внутренний заголовок содержимого';
-      const node = createModalBody(innerTitleText);
-
-      modal.setBody(node);
-
-      expect(selectInnerTitle()).toBe(innerTitleText);
-    });
-  });
-
-  describe('закрытие', () => {
-    let clickEvent;
-    let closeButton;
-
-    beforeEach(() => {
-      modal.open();
-
-      clickEvent = new MouseEvent('click', { bubbles: true });
-      closeButton = document.body.querySelector('.modal .modal__close');
-    });
-
-    it('при вызове метода close(), вёрстка модального окна должна удаляться из body', () => {
-      modal.close();
-
-      let modalElement = document.querySelector('.modal');
-
-      expect(modalElement).toBeNull();
-    });
-
-    it('при вызове метода close(), должен удаляться класс "is-modal-open" с элемента body', () => {
-      modal.close();
-
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
-
-      expect(hasIsModalOpenClass).toBe(false);
-    });
-
-    it('при клике по [X], вёрстка модального окна должна удаляться из body', () => {
-      closeButton.dispatchEvent(clickEvent);
-
-      let modalElement = document.querySelector('.modal');
-
-      expect(modalElement).toBeNull();
-    });
-
-    it('при клике по [X], должен удаляться класс "is-modal-open" с элемента body', () => {
-      closeButton.dispatchEvent(clickEvent);
-
-      let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
-
-      expect(hasIsModalOpenClass).toBe(false);
-    });
-
-    describe('если была нажата клавиша ESC', () => {
-      let escKeyDownEvent;
-
-      beforeEach(() => {
-        escKeyDownEvent = new KeyboardEvent('keydown', { code: 'Escape', key: 'Escape', bubbles: true });
-      });
-
-      it('вёрстка модального окна должна удаляться из body', () => {
-        document.body.dispatchEvent(escKeyDownEvent);
-
-        let modalElement = document.querySelector('.modal');
-
-        expect(modalElement).toBeNull();
-      });
-
-      it('должен удаляться класс "is-modal-open" с элемента body', () => {
-        document.body.dispatchEvent(escKeyDownEvent);
-
-        let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
-
-        expect(hasIsModalOpenClass).toBe(false);
-      });
-    });
-
-    describe('если была нажата любая другая клавиша кроме ESC', () => {
-      let spaceButtonKeyDownEvent;
-
-      beforeEach(() => {
-        spaceButtonKeyDownEvent = new KeyboardEvent('keydown', { code: ' ', key: 'Space', bubbles: true });
-      });
-
-      it('вёрстка модального окна НЕ должна удаляться из body', () => {
-        document.body.dispatchEvent(spaceButtonKeyDownEvent);
-
-        let modalElement = document.querySelector('.modal');
-
-        expect(modalElement).not.toBeNull();
-      });
-
-      it('НЕ должен удаляться класс "is-modal-open" с элемента body', () => {
-        document.body.dispatchEvent(spaceButtonKeyDownEvent);
-
-        let hasIsModalOpenClass = document.body.classList.contains('is-modal-open');
-
-        expect(hasIsModalOpenClass).toBe(true);
-      });
-    });
-
-  });
-});
+import createElement from '../../assets/lib/create-element.js';
+
+export default class Modal {
+  constructor() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+
+    this.body = this.elem.querySelector('.modal__body');
+  }
+
+  addEventListeners() {
+    this.elem.querySelector('.modal__close').addEventListener('click', () => this.close());
+    document.addEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  open() {
+    document.body.appendChild(this.elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  setTitle(title) {
+    this.elem.querySelector('.modal__title').textContent = title;
+  }
+
+  setBody(node) {
+    this.body.innerHTML = '';
+    this.body.appendChild(node);
+  }
+
+  close() {
+    document.body.removeChild(this.elem);
+    document.body.classList.remove('is-modal-open');
+    document.removeEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  handleKeyDown(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+}
